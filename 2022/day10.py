4fd1432316c43a65e44bfa6122b437be2fa5829a
t1emp0 <rafa.beaus@gmail.com>
2022-12-10 14:16:10 +0100

    AoC Day 10: Look what @nauqs made me do (oneliner)

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,4 +1,5 @@
 from utils import get_input
+from functools import reduce
 
 
 def part1(ops):
@@ -11,6 +12,30 @@ def part1(ops):
             if cycle % 40 != 0 and cycle % 20 == 0:
                 strength += cycle * reg
         reg += int(instr[1]) if len(instr) > 1 else 0
+
+    print("Total strength:", strength)
+
+
+def part1_oneliner(ops):
+    print("Total strength:", reduce(lambda old, new: (old[0]+ ((new[0] + 1) * old[1] if (new[0] + 1) % 40 != 0 and (new[0] + 1) % 20 == 0 else 0), old[1] + new[1]), enumerate(ops), (0, 1))[0])
+
+
+def part1_oneliner_expanded(ops):
+    # old = (strength, reg)
+    # new = (cycle-1, instr)
+    strength = reduce(
+        lambda old, new: (
+            old[0]
+            + (
+                (new[0] + 1) * old[1]
+                if (new[0] + 1) % 40 != 0 and (new[0] + 1) % 20 == 0
+                else 0
+            ),
+            old[1] + new[1],
+        ),
+        enumerate(ops),
+        (0, 1),
+    )[0]
 
     print("Total strength:", strength)
 
@@ -38,3 +63,6 @@ if __name__ == "__main__":
     ops = [d.split(" ") for d in data]
     part1(ops)
     part2(ops)
+
+    ops_oneliner = sum([[0] + [int(d.split(" ")[1])] if len(d.split(" ")) == 2 else [0] for d in data], [])
+    part1_oneliner(ops_oneliner)
